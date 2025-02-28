e86665c52f08d563d7e911118a6d6401b734a4a8
FarhanAliQureshi <52462581+FarhanAliQureshi@users.noreply.github.com>
2025-03-01 03:37:45 +0500

    Display CSV data in HTML table

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const csvInput = document.querySelector("#csvinput");
 csvInput.addEventListener("change", updateCsvTable);
+const csvTable = document.querySelector("#csvtable");
 
 function updateCsvTable() {
   const selectedFiles = csvInput.files;
@@ -13,7 +14,45 @@ function updateCsvTable() {
       skipEmptyLines: true,
       complete: function (results) {
         console.log(results);
+        clearTable(csvTable);
+        addDataToTable(results.data, csvTable);
       },
     });
+  }
+}
+
+function clearTable(table) {
+  const rows = table.rows;
+  for (x = rows.length - 1; x >= 0; x--) {
+    table.deleteRow(x);
+  }
+  table.tBodies[0].remove();
+  table.deleteTHead();
+}
+
+function addDataToTable(data, table) {
+  // Add CSV rows and cells to Table rows and cells
+  let headerAdded = false;
+  const newBody = table.createTBody();
+  for (const row of data) {
+    if (headerAdded === false) {
+      addHeaderToTable(row, table);
+      headerAdded = true;
+    } else {
+      let newRow = newBody.insertRow();
+      for (const cell of row) {
+        let newCell = newRow.insertCell();
+        newCell.innerText = cell;
+      }
+    }
+  }
+}
+
+function addHeaderToTable(headerRow, table) {
+  let newHeader = table.createTHead();
+  let newRow = newHeader.insertRow();
+  for (const column of headerRow) {
+    let newColumn = newRow.insertCell();
+    newColumn.innerText = column;
   }
 }
