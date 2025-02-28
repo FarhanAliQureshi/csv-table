d67e3ae23f291ee1b459b1dac781c37912d43d7c
FarhanAliQureshi <52462581+FarhanAliQureshi@users.noreply.github.com>
2025-03-01 01:57:00 +0500

    Initial tracking changes for file input

---
+++ script.js
@@ -0,0 +1,13 @@
+const csvInput = document.querySelector("#csvinput");
+csvInput.addEventListener("change", updateCsvTable);
+
+function updateCsvTable() {
+  const selectedFiles = csvInput.files;
+  if (selectedFiles.length === 0) {
+    // No file selected
+    console.log("No file selected");
+  } else {
+    const currentFile = selectedFiles[0];
+    console.log(`File selected: ${currentFile.name}`);
+  }
+}
